c23b69c28e7584576283dbcadfdda46965a681be
luismiguelgomez <gomezpaezluismiguel@gmail.com>
2021-12-02 01:51:26 -0500

    Eliminando codigo innecesario

--- app/Http/Controllers/InfoPersonalController.php
+++ app/Http/Controllers/InfoPersonalController.php
@@ -8,12 +8,6 @@ use App\Models\Empleado;
 class InfoPersonalController extends Controller
 {
     public function reportTodos(){
-        /*
-        $nombreCompleto = Empleado::select('Nombre_emp', 'Codigo', 'id_dep')
-        ->with(['job' => function($query) {
-        $query->select('id_dep', 'nombre_dep');
-        }])->get();
-        */
          $empleados = \DB::select("SELECT CONCAT(empleado.Nombre_emp, ' ' , empleado.Codigo) nombre_codigo, dependencia.nombre_dep, empleado.Cargo, empleado.Fecha_ingreso, empleado.EPS, empleado.ARL, empleado.Sueldo FROM `empleado` INNER JOIN dependencia ON empleado.id_dep=dependencia.id_dep ORDER BY empleado.Nombre_emp");
 
         $response = [
@@ -26,12 +20,6 @@ class InfoPersonalController extends Controller
     }
 
     public function reportIndividual($nombre){
-        /*
-        $nombreCompleto = Empleado::select('Nombre_emp', 'Codigo', 'id_dep')
-        ->with(['job' => function($query) {
-        $query->select('id_dep', 'nombre_dep');
-        }])->get();
-        */
          $empleados = \DB::select("SELECT CONCAT(empleado.Nombre_emp, ' ' , empleado.Codigo) nombre_codigo, dependencia.nombre_dep, empleado.Cargo, empleado.Fecha_ingreso, empleado.EPS, empleado.ARL, empleado.Sueldo FROM `empleado` INNER JOIN dependencia ON empleado.id_dep=dependencia.id_dep WHERE empleado.Nombre_emp = '".$nombre."' ORDER BY empleado.Nombre_emp");
 
         $response = [
